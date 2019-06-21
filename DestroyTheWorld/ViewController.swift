d2d61686d79ddaa094fce4df88a3638c28e1528d
Cole Cooper <ccooper@ncompasstrac.com>
2019-06-21 14:02:51 -0700

    Added Xcode project.

---
+++ DestroyTheWorld/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld
+//
+//  Created by Cole Cooper on 6/21/19.
+//  Copyright © 2019 Cole Cooper. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        print("Hello World")
+    }
+
+
+}
+
